2a247e2dd2914960043cc81465989794ac5430de
soare23 <toma.mihai.sorin23@gmail.com>
2021-02-04 17:30:48 +0200

    login page WOP

---
+++ Client/real_time_chat_client/src/components/Login.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+function Login() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <div className={classes.margin}>
+        <Grid container spacing={1} alignItems="flex-end">
+          <Grid item>
+            <AccountCircle />
+          </Grid>
+          <Grid item>
+            <TextField id="input-with-icon-grid" label="Username" />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
